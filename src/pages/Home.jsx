d383cf56a74f81f5d746947b9b8f8ecfbfe5651f
adithya519b <adithya462005@gmail.com>
2026-08-18 11:02:24 +0530

    Fix Home page and GitHub Pages deployment

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
-import SearchSection from "../components/SearchSection";
-import LatestMaterials from "../components/LatestMaterials";
+//import SearchSection from "../components/SearchSection";
+//import LatestMaterials from "../components/LatestMaterials";
 import Footer from "../components/Footer";
 
 function Home() {
